649bc7f42c4be4644824cca42ed5cdd87c3253e6
caiouehara <caioueharamartins@gmail.com>
2019-10-18 10:05:14 -0300

    Create Interface

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,13 +1,19 @@
 import player from './player'
 import scenario from './scenario/main'
 const canvas = document.querySelector('canvas')
+const interfaceElement = document.querySelector('#interface')
 let c = canvas.getContext('2d')
 
 let game = {
+    interface:{
+        userPoints: 0,
+    },
+
     init() {
         canvas.width = 800;
         canvas.height = 600;
         console.log(canvas)
+        this.handleInterface()
         this.handleKeyBoard()
         this.animate()
         
@@ -18,6 +24,14 @@ let game = {
             player.move(e.key);
             player.drawCollision()
         })
+    },
+
+    handleInterface(){
+        let h2Element = interfaceElement.children[0];
+        window.setInterval(()=>{
+            this.interface.userPoints += 1;
+            h2Element.innerHTML = this.interface.userPoints;
+        }, 1000)
     },
 
     animate() {
