ec72cf9767dcabe57458a569a1a118c910303b36
caiouehara <caioueharamartins@gmail.com>
2019-10-21 22:26:02 -0300

    Refact Enemys difficulty logic

--- src/js/scenario/main.js
+++ src/js/scenario/main.js
@@ -3,8 +3,9 @@ import player from '../player'
 const canvas = document.querySelector('canvas')
 
 let scneario = {
-    preventSpawnDistance: 50,
+    preventSpawnDistance: 125,
     enemys: [],
+    enemyRadBetween: [75, 25],
 
     update(){
         this.updateEnemys()
@@ -14,15 +15,15 @@ let scneario = {
     updateEnemys(){
         this.enemys.forEach((currentValue ,index)=>{
             // Define Routes
-            if (index % 2 === 0){
-                currentValue.move(1)
-            }
-            else if(index % 5 === 0){
-                currentValue.move(2)
-            }
-            else{
+            if(index % 5 === 0){
                 currentValue.move()
             }
+            else if (index % 2 === 0){
+                currentValue.move(1)
+            }
+            else{
+                currentValue.move(2)
+            }   
             currentValue.draw()
             currentValue.drawCollision(player)
         })  
@@ -36,7 +37,7 @@ let scneario = {
     spawnEnemy(){
         let spawnPosX = Math.random() * canvas.width
         let spawnPosY = Math.random() * canvas.height
-        let randomRad = Math.random() * (75-25) + 25
+        let randomRad = Math.random() * (this.enemyRadBetween[0] - this.enemyRadBetween[1]) + this.enemyRadBetween[1]
         let distance = this.getDistance(player.posX, player.posY, spawnPosX, spawnPosY)
         if(distance > this.preventSpawnDistance + player.width + randomRad){
             let newEnemy = new EnemyCircle( spawnPosX , spawnPosY, randomRad)
