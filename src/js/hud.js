4ff08e623fd7a5cda3331cc9cbc215252fc6ef29
caiouehara <caioueharamartins@gmail.com>
2019-10-24 20:43:40 -0300

    Add Log

--- src/js/hud.js
+++ src/js/hud.js
@@ -2,13 +2,16 @@ import game from './game'
 import scenario from './scenario/main'
 const buttonStart = document.querySelector('#button-start')
 const buttonDisplayGrid = document.querySelector('#button-display-grid-collision')
-const hudElement = document.querySelector('#hud')
 const scoreboardElement = document.querySelector('h2')
 
 let hud = {
     userPoints: 50,
     pointsReward: 10,
     displayGrid: false,
+    log: {
+        element: document.querySelector("#log"),
+        scoreHistory: [],
+    },
 
     init() {
         buttonStart.addEventListener('click', () => {
@@ -28,6 +31,12 @@ let hud = {
         scoreboardElement.innerHTML = this.userPoints;
     },
 
+    updateLog(){
+        let log = this.log
+        log.scoreHistory.push(`<p> Score: ${this.userPoints} </p>`)
+        log.element.innerHTML = log.scoreHistory.join(' ')
+    },
+
     handleGrid() {
         this.displayGrid = !this.displayGrid
         this.displayGrid ? buttonDisplayGrid.innerHTML = "Turn Off" : buttonDisplayGrid.innerHTML = "Display Grid"
@@ -44,6 +53,7 @@ let hud = {
     },
 
     remake() {
+        this.updateLog()
         this.userPoints = 50
         this.pointsReward = 10
     }
