df8b2b59c104810f436e507dfe3edae6639b3718
caiouehara <caioueharamartins@gmail.com>
2019-10-24 21:24:27 -0300

    Add Html Plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     entry: [
@@ -18,5 +19,10 @@ module.exports = {
             }
           }
         ]
-      }
+      },
+      plugins: [
+        new HtmlWebpackPlugin({
+          template: path.resolve(__dirname, 'src/index.html')
+        })
+      ]
 };
