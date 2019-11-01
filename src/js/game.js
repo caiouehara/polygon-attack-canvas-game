de8bebdd807c8af7e05c8d28b60aefaed3cc7af0
caiouehara <caioueharamartins@gmail.com>
2019-11-01 02:14:26 -0300

    Refact Spawn

--- src/js/game.js
+++ src/js/game.js
@@ -26,7 +26,7 @@ let game = {
         for (let playerID in this.players) {
             const player = this.players[playerID]
             // Checking if all players are alive
-            if(!player.alive){
+            if (!player.alive) {
                 game.stop()
             }
             setWallCollision.call(player)
@@ -37,8 +37,8 @@ let game = {
             enemy.move()
         }
         for (let banana of this.bananas) {
-            setCharCollision.call(banana, banana.width, ()=>{
-                freezeEnemys(1000)
+            setCharCollision.call(banana, banana.width, () => {
+                freezeEnemys(500)
             })
             changeObjDirection.call(banana)
             banana.move()
@@ -54,46 +54,55 @@ let game = {
             this.user.points += this.user.pointsReward;
 
             if (phase1) {
-                this.spawnEnemy()
+                this.spawn('enemy')
                 this.user.pointsReward + 10;
             }
         }, 1000)
     },
 
-    spawnEnemy() {
+    spawn(obj) {
         let enemyRadBetween = [75, 25]
         let preventSpawnDistance = 25
 
         let spawnPosX = Math.random() * canvas.width
         let spawnPosY = Math.random() * canvas.height
         let randomRad = Math.random() * (enemyRadBetween[0] - enemyRadBetween[1]) + enemyRadBetween[1]
+        let bananaWidht = new Banana().width
 
         let isProtect = false
 
         // protecting all players
         for (let player in this.players) {
             let distance = getDistance(player.posX, player.posY, spawnPosX, spawnPosY)
-            isProtect = !(distance > preventSpawnDistance + player.width + randomRad)
+            switch (obj) {
+                case 'enemy':
+                    isProtect = !(distance > preventSpawnDistance + player.width + randomRad)
+                    isProtect ? this.enemys.push(new Enemy(spawnPosX, spawnPosY, randomRad)) && this.setEnemyRoutes() : this.spawn('enemy')
+                    break
+                case 'banana':
+                    isProtect = !(distance > preventSpawnDistance + player.width + bananaWidht )
+                    isProtect ? this.bananas.push(new Banana(spawnPosX, spawnPosY)) : this.spawn('banana')
+                    break
+            }
         }
-
-        isProtect ? this.enemys.push(new Enemy(spawnPosX, spawnPosY, randomRad)) && this.setEnemyRoutes() : this.spawnEnemy()
     },
 
     setEnemyRoutes() {
-        this.enemys.forEach((cv, index) => {
-            if (index % 5 === 0) {
-                cv.routeType = 0;
+        for (let enemyID in this.enemys) {
+            const enemy = this.enemys[enemyID]
+            if (enemyID % 5 === 0) {
+                enemy.routeType = 0
             }
-            else if (index % 2 === 0) {
-                cv.routeType = 1
+            else if (enemyID % 2 === 0) {
+                enemy.routeType = 1
             }
             else {
-                cv.routeType = 2
+                enemy.routeType = 2
             }
-        })
+        }
     },
 
-    stop(){
+    stop() {
         // stop loops
         this.running = false // stop animationFrame loop 
         clearInterval(pointTimer)
@@ -101,7 +110,7 @@ let game = {
         this.hud.scoreHistory.push(`<p> Score: ${this.user.points} </p>`)
         // remake
         this.user = { points: 10, pointsReward: 30 },
-        this.enemys = [ new Enemy(200, 200, 25)]
+            this.enemys = [new Enemy(200, 200, 25)]
         this.players.player1 = new Player() // have to change to userID
     }
 }
